23790df8048b9eb828d7c20ac9e536548b650283
Notorious <notorious_d_e_v@proton.me>
2025-03-13 02:58:55 +0000

    feat: create an MPC Server for discovering other MPC servers related to Solana

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import serversTextResponse from "./servers.js";
+
+// Create server instance
+const server = new McpServer({
+  name: "Solana MCP Explorer",
+  version: "1.0.0",
+});
+
+// Add a resource to list all Solana related MCP Servers
+server.resource(
+  "explore",
+  "explore://solana",
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: serversTextResponse,
+    }]
+  })
+);
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
---
+++ src/servers.js
@@ -0,0 +1,33 @@
+// list your mcp servers here
+// Example:
+//
+
+const mcpServers = [
+    {
+        "name": "Solana Agent Kit MCP Server",
+        "description": "A Model Context Protocol (MCP) server that provides onchain tools for Claude AI. This allows Claude to interact with the Solana blockchain, execute transactions, query account information, and manage Solana wallets.",
+        "homepage": "https://github.com/sendaifun/solana-mcp"
+    },
+    {
+        "name": "Solana MCP Explorer",
+        "description": "Discover all the MCPs on Solana",
+        "homepage": "https://github.com/notorious-d-e-v/solana-mcp-explorer",
+    },
+    {
+        "name": "Solana Model Context Protocol (MCP) Demo",
+        "description": "This demo showcases a simple MCP server implementation that provides basic RPC methods for Solana (getBalance, getAccountInfo, getTransaction), and some helpful prompts for Solana development.",
+        "homepage": "https://github.com/solana-foundation/solana-dev-mcp"
+    }
+]
+
+
+const sortedByName = mcpServers.sort((a, b) => a.name.localeCompare(b.name))
+
+
+let serversTextResponse = "Check out these MCP servers on Solana!\n"
+sortedByName.forEach((server) => {
+    serversTextResponse += `\n${server.name}\n${server.description}\n${server.homepage}\n\n`
+})
+
+
+export default serversTextResponse
